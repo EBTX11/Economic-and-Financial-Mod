de50bf91bda1a3512b657c71321189295560a5c9
EBTX11 <139619681+EBTX11@users.noreply.github.com>
2024-01-23 16:32:54 +0100

    Delete Economic and Financial Mod (E&F) - V2 directory

--- Economic and Financial Mod (E&F) - V2/localization/program.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-node -v > nul 2>&1
-IF %ERRORLEVEL% EQU 0 (
-    start cmd.exe /k "node app.js"
-) ELSE (
-    echo Node.js is not installed. Please install Node.js and try again.
-    pause
-)
